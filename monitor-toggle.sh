e028959b602ddbc0beba42a5c643637e4b134766
Oleg Puzanov <puzanov@gmail.com>
2020-10-18 20:04:12 +0600

    added 3k monitor support

--- monitor-toggle.sh
+++ monitor-toggle.sh
@@ -3,6 +3,11 @@
 EXTERNAL_OUTPUT="HDMI-1"
 INTERNAL_OUTPUT="eDP-1"
 
+# scalings
+# 1.5
+# 1.76
+# 1.333
+
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
   monitor_mode="EXTERNAL"
@@ -19,8 +24,11 @@ else
 	monitor_mode="EXTERNAL"
 	xrandr --output $INTERNAL_OUTPUT --off
 	xrandr --output $EXTERNAL_OUTPUT --auto
-	xrandr --output $EXTERNAL_OUTPUT --mode 1920x1080 --scale 1.5x1.5
-	xrandr --output $EXTERNAL_OUTPUT --mode 1920x1080 --scale 1.5x1.5 --panning 1920x1080
+	# 2560x1440     59.95 +
+	#xrandr --output $EXTERNAL_OUTPUT --mode 1920x1200 --scale 1.5x1.5 --panning 2880x1800 #DELL 24 
+	xrandr --output $EXTERNAL_OUTPUT --mode 2560x1440 --scale 1.5x1.5 --panning 3840x2160 #DELL 27 
+	#xrandr --output $EXTERNAL_OUTPUT --mode 1920x1080 --scale 1.66x1.66 --dpi 272
+	#xrandr --output $EXTERNAL_OUTPUT --mode 1920x1080 --scale 1.66x1.66 --dpi 272 --panning 1920x1080
 fi	
 
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
